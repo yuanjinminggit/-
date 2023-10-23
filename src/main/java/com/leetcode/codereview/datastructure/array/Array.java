44d0e82682ec6f5d6e0e9a2359956fdfeb6474ed
Atguigu <xg114747411@126.com>
2023-10-24 02:25:52 +0800

    动态数组代码提交

---
+++ src/main/java/com/leetcode/codereview/datastructure/array/Array.java
@@ -0,0 +1,146 @@
+package com.leetcode.codereview.datastructure.array;
+
+public class Array<E> {
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void add(int index, E e) {
+        // 保证数组中的值连续
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("异常");
+        }
+        if (size == data.length) {
+            reSize(2 * data.length);
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    private void reSize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public E remove(int index) {
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;
+
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            reSize(data.length / 2);
+        }
+        return ret;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("array: size = %d,capacity = %d \n", size, data.length));
+        sb.append('[');
+        for (int i = 0; i < size; i++) {
+            sb.append(data[i]);
+            if (i != size - 1) {
+                sb.append(',');
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    E get(int index) {
+        return data[index];
+    }
+
+    void set(int index, E e) {
+        data[index] = e;
+    }
+
+    public static void main(String[] args) {
+
+        Array<Integer> array = new Array<>(10);
+        for (int i = 0; i < 10; i++) {
+            array.addLast(i);
+        }
+        array.remove(1);
+        array.remove(1);
+        array.remove(1);
+        array.remove(1);
+        array.remove(1);
+        System.out.println(array);
+
+    }
+
+}
---
+++ src/main/java/com/leetcode/codereview/tree/Xiangtongdeshu.java
@@ -0,0 +1,30 @@
+package com.leetcode.codereview.tree;
+
+import com.leetcode.codereview.simpleConstruct.TreeNode;
+
+public class Xiangtongdeshu {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
