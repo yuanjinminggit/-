e1308c72c9a871647da04d06f5597cb9d2d52a70
yjm <1779569744@qq.com>
2023-12-10 13:34:03 +0800

    分之1.2

--- src/main/java/com/leetcode/codereview/CodeReviewApplication.java
+++ src/main/java/com/leetcode/codereview/CodeReviewApplication.java
@@ -8,6 +8,7 @@ public class CodeReviewApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CodeReviewApplication.class, args);
+        System.out.println("yjm1.2分之");
     }
 
 }
